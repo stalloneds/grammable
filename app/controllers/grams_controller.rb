b18e093388c10fb9ad97047303f3bf795ce0a1a8
Stallone Dias <stalloneds@gmail.com>
2019-07-06 01:13:14 +0000

    added destroy functionality

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -11,7 +11,6 @@ class GramsController < ApplicationController
 	def create
 		@gram = current_user.grams.create(gram_params)
 		
-
 		if @gram.valid?
 			redirect_to root_path
 		else
@@ -19,9 +18,43 @@ class GramsController < ApplicationController
 		end 
 	end 
 
+	def show
+		@gram = Gram.find_by_id(params[:id])
+  		return render_not_found if @gram.blank?
+	end 
+
+	def edit
+		@gram = Gram.find_by_id(params[:id])
+		return render_not_found if @gram.blank?
+	end 
+
+	def update
+    	@gram = Gram.find_by_id(params[:id])
+  		return render_not_found if @gram.blank?
+
+  		@gram.update_attributes(gram_params)
+
+  		if @gram.valid?
+    		redirect_to root_path
+  		else
+    		return render :edit, status: :unprocessable_entity
+  		end
+  	end
+
+  	def destroy
+  		@gram = Gram.find_by_id(params[:id])
+  		return render_not_found if @gram.blank?
+  		@gram.destroy
+  		redirect_to root_path
+	end
+
 	private
 
 	def gram_params
 		params.require(:gram).permit(:message)
+	end 
+
+	def render_not_found
+		render plain: 'Not Found :(', status: :not_found
 	end 
 end
